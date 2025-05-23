a888e076348b7fc8ec04fa83756399ddf5942ccc
Qabil <leancyleancy5@gmail.com>
2025-05-24 01:17:51 +0700

    Homepage javascript

---
+++ script/home.js
@@ -0,0 +1,31 @@
+const kontrakanData = [
+    {
+    title: "KONTRAKAN PETAKAN",
+    image: "/image/pexels-pixabay-358636.jpg"
+    },
+    {
+    title: "KONTRAKAN RUMAH",
+    image: "/image/pexels-pixabay-358636.jpg"
+    },
+    {
+    title: "PARKIRAN PETAKAN",
+    image: "/image/pexels-pixabay-358636.jpg"
+    }
+];
+
+const container = document.getElementById("type-card-container");
+
+kontrakanData.forEach(typeData => {
+
+    const card = document.createElement("div");
+    card.className = "type-card";
+    card.innerHTML = 
+            `<div  class="sub-title">
+                <h5>${typeData.title}</h5>
+            </div>
+    
+            <div class="sub-image">
+                <img class="sub-image-control" src="${typeData.image}" alt="${typeData.title}">
+            </div>`;
+    container.appendChild(card);
+});
