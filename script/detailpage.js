49a731ab2f055794c09c2eaf20b14cd4ffaa49ce
Qabil <leancyleancy5@gmail.com>
2025-05-25 08:20:04 +0700

    perubahan detail page v1

--- script/detailpage.js
+++ script/detailpage.js
@@ -1,3 +1,33 @@
 const storedData = JSON.parse(sessionStorage.getItem("selectedData"));
 document.getElementById("detail-title").textContent = storedData.title;
 document.getElementById("detail-image").src = storedData.image;
+
+
+const imagesRuangan = {
+    "5A" : [
+        "../image/Я же психолог, а не копирайтер.jpg",
+        "../image/Я же психолог, а не копирайтер.jpg",
+        "../image/Я же психолог, а не копирайтер.jpg"
+    ],
+    "5B" : [
+        "../image/roberto-nickson-FqHU6aeVwf8-unsplash.jpg",
+        "../image/roberto-nickson-FqHU6aeVwf8-unsplash.jpg",
+        "../image/roberto-nickson-FqHU6aeVwf8-unsplash.jpg"
+    ]
+};
+
+const buttons = document.querySelectorAll(".filter-button button");
+const imgs = document.querySelectorAll(".detail-img-control");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const room = button.dataset.room;
+    const images = imagesRuangan[room];
+
+    if (images) {
+      imgs.forEach((img, i) => {
+        img.src = images[i] || "";
+      });
+    }
+  });
+});
