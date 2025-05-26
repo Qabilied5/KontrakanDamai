bab90cf841e8e78a8849f604a5e760e7f488f3bd
Qabil <leancyleancy5@gmail.com>
2025-05-26 07:45:39 +0700

    perubahan pagedetail dan contact container

--- script/home.js
+++ script/home.js
@@ -1,25 +1,28 @@
 const kontrakanData = [
     {
     title: "KONTRAKAN PETAKAN",
-    image: "../image/pexels-pixabay-358636.jpg"
-    },
-    {
+    type: "petakan",
+    image: "../image/PETAKAN-VIEW.jpg"
+  },
+  {
     title: "KONTRAKAN RUMAH",
+    type: "rumah",
     image: "../image/pexels-pixabay-358636.jpg"
-    },
-    {
+  },
+  {
     title: "PARKIRAN PETAKAN",
+    type: "parkiran",
     image: "../image/pexels-pixabay-358636.jpg"
-    }
+  }
 ];
 
 const container = document.getElementById("type-card-container");
 
 kontrakanData.forEach(typeData => {
 
-    const card = document.createElement("div");
-    card.className = "type-card";
-    card.innerHTML =
+        const card = document.createElement("div");
+        card.className = "type-card";
+        card.innerHTML =
             `<div  class="sub-title">
                 <h5>${typeData.title}</h5>
             </div>
@@ -28,17 +31,26 @@ kontrakanData.forEach(typeData => {
                 <img class="sub-image-control" src="${typeData.image}" alt="${typeData.title}">
             </div>`;
             
-            const imgBtn = card.querySelector(".sub-image");
+        const imgBtn = card.querySelector(".sub-image");
             
         imgBtn.addEventListener("click", () => {
         const dataTemp = {
         title: typeData.title,
-        image: typeData.image
+        image: typeData.image,
+        type: typeData.type
         };
 
         sessionStorage.setItem("selectedData", JSON.stringify(dataTemp));
 
-        window.location.href = "../pages/detailpage.html"
+        if (typeData.type === "petakan") {
+            targetPage = "../pages/detailkontrakan-petakan.html";
+        } else if (typeData.type === "rumah") {
+            targetPage = "../pages/detailkontrakan-rumah.html";
+        } else if (typeData.type === "parkiran") {
+            targetPage = "../pages/detailparkiran.html";
+        }
+        console.log(typeData.type);
+        window.location.href = targetPage;
     });
     container.appendChild(card);
 });
